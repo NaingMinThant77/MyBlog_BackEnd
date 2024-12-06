721b45ae633235613d9894bee7fe075f4a2f0754
NaingMinThant77 <naingminthant07@gmail.com>
2024-12-06 14:36:59 +0630

    expressValidator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const User = require("../models/user")
 
 const crypto = require("crypto");
 
+const { validationResult } = require("express-validator");
+
 const dotenv = require("dotenv").config();
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
@@ -15,52 +17,81 @@ const transporter = nodemailer.createTransport({
 
 //render register page
 exports.getRegisterPage = (req, res) => {
-    res.render("auth/register", { title: "RegisterPage", errorMsg: req.flash("error")[0] })
+    res.render("auth/register", {
+        title: "RegisterPage",
+        errorMsg: req.flash("error")[0],
+        oldFormData: { email: "", password: "" }
+    })
 }
 
 //handle register
 exports.registerAccount = (req, res) => {
     const { email, password } = req.body;
-    User.findOne({ email }).then(
-        user => {
-            if (user) {
-                req.flash("error", "Check your information and Try again")
-                return res.redirect("/register")
-            }
-            return bcrypt.hash(password, 10).then(hashedPass => {
-                return User.create({
-                    email,
-                    password: hashedPass
-                })
-            }).then(_ => {
-                res.redirect("/login")
-                transporter.sendMail({
-                    from: "process.env.SENDER_MAIL",
-                    to: email,
-                    subject: "Login successful",
-                    text: "Your message here",
-                    html: "<h1>Register account successfully</h1><P>Create an account using this email address in blog.io.</P>"
-                }, (err => { console.log(err) }))
-                // to reduce security on process.env.SENDER_MAIL
-                // turn on 2-step verification on email and add "App passwords(Mail, device - nodemailer) and will get string change with that string in MAIL_PASSWORD "
-                //test with tempMail website(will get mail)
-            })
-        }).catch(err => console.log(err))
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) { //422 - validation
+        return res.status(422).render("auth/register", {
+            title: "RegisterPage",
+            errorMsg: errors.array()[0].msg,
+            oldFormData: { email, password }
+        })
+    }
+
+    // User.findOne({ email }).then(
+    //     user => {
+    // if (user) {
+    //     req.flash("error", "Check your information and Try again")
+    //     return res.redirect("/register")
+    // } return
+    bcrypt.hash(password, 10).then(hashedPass => {
+        return User.create({
+            email,
+            password: hashedPass
+        })
+    }).then(_ => {
+        res.redirect("/login")
+        // transporter.sendMail({
+        //     from: "process.env.SENDER_MAIL",
+        //     to: email,
+        //     subject: "Login successful",
+        //     text: "Your message here",
+        //     html: "<h1>Register account successfully</h1><P>Create an account using this email address in blog.io.</P>"
+        // }, (err => { console.log(err) }))
+        // to reduce security on process.env.SENDER_MAIL
+        // turn on 2-step verification on email and add "App passwords(Mail, device - nodemailer) and will get string change with that string in MAIL_PASSWORD "
+        //test with tempMail website(will get mail)
+    })
+    // }).catch(err => console.log(err))
 }
 
 //render login page
 exports.getLoginPage = (req, res) => {
-    res.render("auth/login", { title: "LoginPage", errorMsg: req.flash("error")[0] });
+    res.render("auth/login", { title: "LoginPage", errorMsg: req.flash("error")[0], oldFormData: { email: "", password: "" } });
 }
 
 //handle login
 exports.postLoginData = (req, res) => {
     const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render("auth/login", {
+            title: "LoginPage",
+            errorMsg: errors.array()[0].msg,
+            oldFormData: { email, password }
+        })
+    }
+
     User.findOne({ email }).then(
         user => {
             if (!user) {  //key, value
-                req.flash("error", "Check your information and Try again")
-                return res.redirect("/login")
+                // req.flash("error", "Check your information and Try again")
+                // return res.redirect("/login")
+                return res.status(422).render("auth/login", {
+                    title: "LoginPage",
+                    errorMsg: "Please enter valid mail and password",
+                    oldFormData: { email, password }
+                })
             }
             bcrypt.compare(password, user.password).then(
                 isMatch => {
@@ -69,10 +100,15 @@ exports.postLoginData = (req, res) => {
                         req.session.userInfo = user;
                         return req.session.save(err => {
                             res.redirect("/")
-                            console.log(err)
+                            // console.log(err)
                         });
                     }
-                    res.redirect("/login")
+                    // res.redirect("/login")
+                    res.status(422).render("auth/login", {
+                        title: "LoginPage",
+                        errorMsg: "Please enter valid mail and password",
+                        oldFormData: { email, password }
+                    })
                 }
             ).catch(err => console.log(err))
         }).catch(err => console.log(err))
@@ -129,7 +165,13 @@ exports.getNewPasswordPage = (req, res) => {
     const { token } = req.params;
     User.findOne({ resetToken: token, tokenExpiration: { $gt: Date.now() } }).then(user => {
         if (user) {
-            res.render("auth/newPassword", { title: "ChangePassword", errorMsg: req.flash("error")[0], resetToken: token, user_id: user._id.toString() })
+            res.render("auth/newPassword", {
+                title: "ChangePassword",
+                errorMsg: req.flash("error")[0],
+                resetToken: token,
+                user_id: user._id.toString(),
+                oldFormData: { password: "", confirmedpassword: "" }
+            })
         } else {
             res.redirect("/expirationPage")
         }
@@ -138,14 +180,24 @@ exports.getNewPasswordPage = (req, res) => {
 
 exports.changeNewPassword = (req, res) => {
     const { password, confirmedpassword, user_id, resetToken } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render("auth/newPassword", {
+            title: "ChangePassword",
+            resetToken,
+            user_id: user_id.toString(),
+            errorMsg: errors.array()[0].msg,
+            oldFormData: { password, confirmedpassword }
+        })
+    }
+
     let resetUser;
     User.findOne({ resetToken, tokenExpiration: { $gt: Date.now() }, _id: user_id }).then(user => {
-        if (password === confirmedpassword) {
-            resetUser = user;
-            return bcrypt.hash(password, 10)
-        } else {
-
-        }
+        // if (password === confirmedpassword) {
+        resetUser = user;
+        return bcrypt.hash(password, 10)
+        // }
     }).then(
         hashPassword => {
             resetUser.password = hashPassword;
@@ -155,7 +207,7 @@ exports.changeNewPassword = (req, res) => {
         }
     ).then(
         _ => {
-            res.redirect("/login");
+            return res.redirect("/login");
         }
     ).catch(err => console.log(err))
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,24 @@
 const express = require("express")
 const router = express.Router();
 const authController = require("../controllers/auth")
+const { body } = require("express-validator");
+const User = require("../models/user");
+const { Error } = require("mongoose");
 
 router.get("/register", authController.getRegisterPage);
-router.post("/register", authController.registerAccount);
+
+router.post("/register", body("email").isEmail().withMessage("Please enter a valid email address").custom((value, { req }) => { //async validation
+    return User.findOne({ email: value }).then(
+        user => {
+            if (user) {
+                return Promise.reject("Email is already existed. Please enter another one.")
+            }
+        })
+}), body("password").isLength({ min: 4 }).trim().withMessage("Password must have 5 characters."), authController.registerAccount);
+
 
 router.get("/login", authController.getLoginPage);
-router.post("/login", authController.postLoginData);
+router.post("/login", body("email").isEmail().withMessage("Please enter a valid email address"), body("password").isLength({ min: 4 }).trim().withMessage("Password must valid"), authController.postLoginData);
 
 router.post("/logout", authController.logout);
 
@@ -18,7 +30,12 @@ router.post("/reset", authController.resetLinkSend)
 
 router.get("/reset-password/:token", authController.getNewPasswordPage)
 
-router.post("/change-newpassword", authController.changeNewPassword)
+router.post("/change-newpassword", body("password").isLength({ min: 4 }).trim().withMessage("Password must valid"), body("confirmedpassword").trim().custom((value, { req }) => {
+    if (value !== req.body.password) {
+        throw new Error("Password must match!!!!")
+    }
+    return true;
+}), authController.changeNewPassword)
 
 router.get("/expirationPage", authController.getExperiationPage)
 module.exports = router
